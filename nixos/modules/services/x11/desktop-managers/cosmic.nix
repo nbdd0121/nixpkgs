19ff2c4c4b3c11284dbe92173a785b5a1a003eea
Niko <97130632+Nyabinary@users.noreply.github.com>
2023-11-12 15:54:30 -0500

    nixos/cosmic: init

---
+++ nixos/modules/services/x11/desktop-managers/cosmic.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, lib, utils, ... }:
+
+let
+  cfg = config.services.xserver.desktopManager.cosmic;
+in
+{
+  meta.maintainers = with lib.maintainers; [ nyanbinary ];
+
+  options.services.xserver.desktopManager.cosmic = {
+    enable = lib.mkEnableOption (lib.mdDoc "COSMIC desktop environment");
+  };
+
+  config = lib.mkIf cfg.enable {
+    # components that need to be in PATH
+    environment.systemPackages = with pkgs; [
+      cosmic-applibrary
+      cosmic-applets
+      cosmic-bg
+      cosmic-comp
+      cosmic-greeter
+      cosmic-icons
+      cosmic-launcher
+      cosmic-notifications
+      cosmic-osd
+      cosmic-panel
+      cosmic-screenshot
+      cosmic-settings
+      cosmic-settings-daemon
+      cosmic-workspaces-epoch
+      xdg-desktop-portal-cosmic
+    ];
+
+    # session files for display manager and systemd
+    services.xserver.displayManager.sessionPackages = with pkgs; [ cosmic-session ];
+    systemd.packages = with pkgs; [ cosmic-session ];
+  };
+}
